a0124a903b0b9944946c8681b336615b5b962a74
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-16 11:08:34 -0600

    update

--- src/models/itineraryModel.js
+++ src/models/itineraryModel.js
@@ -1,12 +1,12 @@
 const pool = require('../config/db'); 
 
 const getAllItineraries = async () => {
-    const result = await pool.query('SELECT * FROM itineraries');
+    const result = await pool.query('SELECT * FROM itinerary');
     return result.rows;
 };
 
 const getItineraryById = async (itineraryId) => {
-    const result = await pool.query('SELECT * FROM itineraries WHERE id = $1', [itineraryId]);
+    const result = await pool.query('SELECT * FROM itinerary WHERE id = $1', [itineraryId]);
     return result.rows[0];
 };
 
@@ -15,7 +15,7 @@ const addItinerary = async (itineraryData) => {
     const startDateTime = `${startDate} ${startTime}`;
     const endDateTime = `${endDate} ${endTime}`;
     const result = await pool.query(
-        'INSERT INTO itineraries (event_name, location, start_datetime, end_datetime, description, notification) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+        'INSERT INTO itinerary (event_name, location, start_datetime, end_datetime, description, notification) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
         [eventName, location, startDateTime, endDateTime, description, notification]
     );
     return result.rows[0];
@@ -26,14 +26,14 @@ const updateItinerary = async (itineraryId, itineraryData) => {
     const startDateTime = `${startDate} ${startTime}`;
     const endDateTime = `${endDate} ${endTime}`;
     const result = await pool.query(
-        'UPDATE itineraries SET event_name = $1, location = $2, start_datetime = $3, end_datetime = $4, description = $5, notification = $6 WHERE id = $7 RETURNING *',
+        'UPDATE itinerary SET event_name = $1, location = $2, start_datetime = $3, end_datetime = $4, description = $5, notification = $6 WHERE id = $7 RETURNING *',
         [eventName, location, startDateTime, endDateTime, description, notification, itineraryId]
     );
     return result.rows[0];
 };
 
 const deleteItinerary = async (itineraryId) => {
-    const result = await pool.query('DELETE FROM itineraries WHERE id = $1 RETURNING *', [itineraryId]);
+    const result = await pool.query('DELETE FROM itinerary WHERE id = $1 RETURNING *', [itineraryId]);
     return result.rows[0];
 };
 
