453399102e224f6b572f4105463c8ddff494ce03
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-17 04:53:12 -0600

    Update server.js

--- server.js
+++ server.js
@@ -64,4 +64,4 @@ app.get('*', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));v
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
