e2527867f9dcd1a64b87282fd1242ff41adaf1cd
Nicole Merriman <nicole.merriman17@gmail.com>
2023-12-16 22:48:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const itineraryRoutes = require('./routes/itineraryRoutes');
+const itineraryRoutes = require('./src/routes/itineraryRoutes.js');
 require('dotenv').config();
 
 const app = express();
